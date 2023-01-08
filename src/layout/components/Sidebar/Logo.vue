090f02a54f4b68158861f3196e8162e1dec6fae5
王言笙 <11325243+sam-wang0806@user.noreply.gitee.com>
2023-01-09 00:40:14 +0800

    修改了侧边栏

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,8 @@ const logo = ref<string>(
       class="h-[50px] flex items-center justify-center"
       to="/"
     >
-      <img v-if="settingsStore.sidebarLogo" :src="logo" class="w-5 h-5" />
-      <h1 v-else>vue3-element-admin</h1>
+      <img v-if="settingsStore.sidebarLogo" src="@/assets/jnu.jpeg" class="w-5 h-5" />
+      <h1 v-else>区块链实训平台</h1>
     </router-link>
 
     <router-link
@@ -34,8 +34,8 @@ const logo = ref<string>(
       class="h-[50px] flex items-center justify-center"
       to="/"
     >
-      <img v-if="settingsStore.sidebarLogo" :src="logo" class="w-5 h-5" />
-      <span class="ml-3 text-white text-sm font-bold">vue3-element-admin</span>
+      <img v-if="settingsStore.sidebarLogo" src="@/assets/jnu.jpeg" class="w-5 h-5" />
+      <span class="ml-3 text-white text-sm font-bold">区块链实训平台</span>
     </router-link>
   </transition>
 </template>
